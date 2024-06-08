bab4935c1525a08e0fc1864ef376e6f96a1598c0
Alan <Rodriguez>
2024-06-08 12:45:38 +0100

    articles page styled

--- client/src/pages/ArticlesList.jsx
+++ client/src/pages/ArticlesList.jsx
@@ -1,7 +1,32 @@
+import articleContent from './article-content';
+import { Link } from 'react-router-dom';
+
 export default function ArticlesList() {
   return(
-    <h1 className="sm:text-4xl text-2xl font-bold my-6 text-gray-900">
-      All articles
-    </h1>
+    <div>
+      <h1 className="sm:text-4xl text-2xl font-bold my-6 text-gray-900">
+        All articles
+      </h1>
+      <div className="container py-4 mx-auto">
+        <div className="flex flex-wrap -m-4">
+          {articleContent.map((article, index) => 
+          <div key={index} className="p-4 md:w-1/2">
+            <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+              <Link to={`/articles/${article.name}`}>
+                <img className="lg:h-48 md:h-36 w-full object-cover object-center" src={article.thumbnail} alt="Thumbnail" />
+                <div className="p-3">
+                  <h3 className="text-lg font-medium text-gray-900 mb-3">{article.title}</h3>
+                  <p className="leading-string-relaxed">
+                    {article.content[0].substring(0,110)} ...
+                  </p>
+                  <p className="text-indigo-500 inline-flex items-center mt-2 md:mb-2 lg:mb-0">Learn more</p>
+                </div>
+              </Link>
+            </div>
+          </div>
+          )}
+        </div>
+      </div>
+    </div>
   )
 }
