216521ff45f47dfbb4f8083fe0011d7d4f896745
Alan <Rodriguez>
2024-06-08 13:59:29 +0100

    rendering articles links and other articles

--- client/src/pages/Article.jsx
+++ client/src/pages/Article.jsx
@@ -1,18 +1,26 @@
 import { useParams } from "react-router-dom";
 import articleContent from "./article-content.jsx"
+import Articles from "../components/Articles.jsx";
 
 export default function Article() {
   const { name } = useParams();
   const article = articleContent.find((article) => article.name === name)
   if (!article) return <h1>Article does not exist</h1> 
+  const otherArticles = articleContent.filter((article)=> article.name != name)
   return (
-    <div>
+    <>
       <h1 className="sm:text-4xl text-2xl font-bold my-6 text-gray-900">
         {article.title}
       </h1>
       {article.content.map((content, index) => 
         <p className="mx-auto leading-relaxed text-base mb-4" key={index}>{content}</p>
       )}
-    </div>
+      <h1 className="sm:text-2xl text-xl font-bold my-4 text-gray-900">
+        Other articles
+      </h1>
+      <div className="flex flex-wrap -m-4">
+        <Articles articles={otherArticles}/>
+      </div>
+    </>
   )
 }
